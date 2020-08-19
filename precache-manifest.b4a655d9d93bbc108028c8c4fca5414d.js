4749772deef6f6bf4a647c21a49becc1447c4843
Constantin-dot <konstantinkspb@gmail.com>
2020-08-19 12:06:26 +0300

    Updates

---
+++ precache-manifest.b4a655d9d93bbc108028c8c4fca5414d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d39d212279e5882cc864281a5652555",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e2c34739ff78d9a62bac",
+    "url": "/portfolio/static/css/main.57126864.chunk.css"
+  },
+  {
+    "revision": "75a3168844590df7cc67",
+    "url": "/portfolio/static/js/2.eb5aa08e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.eb5aa08e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c34739ff78d9a62bac",
+    "url": "/portfolio/static/js/main.67e4b7ab.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
