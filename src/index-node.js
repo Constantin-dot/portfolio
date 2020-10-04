e1f7ec865ce236da6084538f335d878b773c6353
Constantin-dot <konstantinkspb@gmail.com>
2020-10-04 13:06:42 +0300

    fixed settings of form 3.2

--- src/index-node.js
+++ src/index-node.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://constantin-dot.github.io"
+}));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
